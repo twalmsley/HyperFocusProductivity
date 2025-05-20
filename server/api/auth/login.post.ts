1bfd50c89e022f82a5d72adfe2d10331ee445b59
Tony Walmsley <tony@aosd.co.uk>
2025-05-20 09:43:38 +0100

    Secure cookie for production

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -112,7 +112,7 @@ export default defineEventHandler(async (event: H3Event) => {
       httpOnly: true,
       path: '/',
       sameSite: 'strict',
-      secure: false,
+      secure: process.env.NODE_ENV === 'production',
       maxAge: 7 * 24 * 60 * 60 // 7 days
     })
 
